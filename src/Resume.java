b1ac7e9e4329e4a598c8891a2e18e98f5cc7d369
Renuka-Ratnala <deviratnala13@gmail.com>
2025-12-17 21:58:34 +0530

    Create Resume.java

---
+++ src/Resume.java
@@ -0,0 +1,11 @@
+public class Resume {
+    private String content;
+
+    public Resume(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
