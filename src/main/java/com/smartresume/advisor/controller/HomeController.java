10f69b456d8256ab73bd8bedbacfdacc9fd5b5a4
Renuka-Ratnala <deviratnala13@gmail.com>
2026-06-24 21:56:38 +0530

    Add home endpoint

---
+++ src/main/java/com/smartresume/advisor/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.smartresume.advisor.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Smart Resume Advisor Backend is Running!";
+    }
+}
